afc21c03a8b24261fd4b3ba404c197c09364f22d
khr <hengruikou@gmail.com>
2025-05-20 11:24:06 +0800

    删除不必要的文件

--- src/chain/eth/provider.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-
-export const ethProvider = new ethers.JsonRpcProvider(
-  process.env.ETH_RPC_URL || "https://mainnet.infura.io/v3/YOUR_API_KEY"
-);
--- src/chain/solana/provider.ts
+++
@@ -1,7 +0,0 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
-import { env } from "../../util/env";
-// 设置Solana连接
-export const solConnection = new Connection(
-  env.SOLANA_RPC_URL || clusterApiUrl("mainnet-beta"),
-  "confirmed"
-);
